df2ed03b128e507eb01e41cc14ddfdf4851b083b
Raju Ahammad <raju@hastechit.com>
2019-08-17 10:35:59 +0600

    Update UI

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var holidayContent = document.querySelector(".table-body-content");
+var holidayContent = document.querySelector(".table-body-content"),
+    heading = document.querySelector(".main-title");
 
 // Create Node Element
 function createElem(tag) {
@@ -10,6 +11,12 @@ function appendElem(parent, elem) {
     return parent.appendChild(elem);
 }
 
+// Additional Variable
+var d = new Date(),
+    Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+heading.innerHTML = `Bangladesh Holidays in ${d.getFullYear()}`;
 
 // API Info
 var BASE = "https://calendarific.com/api/v2/holidays?",
@@ -22,25 +29,32 @@ function bdHolidays() {
             var holidaysList = data.response.holidays;
 
             holidaysList.forEach((elem, index) => {
+                // Create Elements
                 let tr = createElem('tr'),
-                    holidaySL = createElem("td"),
                     holidayTitle = createElem("td"),
                     holidayDate = createElem('td'),
-                    holidayType = createElem('td');
-                tr.className = 'holiday-item';
-                holidaySL.className = 'holiday-no';
-                holidayTitle.className = 'holiday-title';
-                holidayDate.className = 'holiday-date';
-                holidayType.className = 'holiday-type';
-                holidaySL.innerHTML = index + 1;
-                holidayTitle.innerHTML = elem.name;
-                holidayDate.innerHTML = `${elem.date.datetime.day}-${elem.date.datetime.month}-${elem.date.datetime.year}`;
-                holidayType.innerHTML = elem.type;
+                    holidayDay = createElem('td'),
+                    holidayDesc = createElem('span');
 
-                appendElem(tr, holidaySL);
+                // Add Class Name 
+                tr.className = 'holiday-item';
+                holidayTitle.className = 'holiday-title';
+                holidayDesc.className = 'holiday-desc';
+                holidayDay.className = 'holiday-weekday';
+                holidayDate.className = 'holiday-date';
+
+                // Insert Data On HTML
+                holidayTitle.innerHTML = elem.name;
+                holidayDesc.innerHTML = elem.description;
+                d = new Date(`${elem.date.datetime.year}-${elem.date.datetime.month}-${elem.date.datetime.day}`);
+                holidayDay.innerHTML = Days[d.getDay()];
+                holidayDate.innerHTML = `${Months[d.getMonth()]} ${elem.date.datetime.day}`;
+
+                // Append Element
                 appendElem(tr, holidayTitle);
+                appendElem(holidayTitle, holidayDesc);
+                appendElem(tr, holidayDay);
                 appendElem(tr, holidayDate);
-                appendElem(tr, holidayType);
                 appendElem(holidayContent, tr);
             });
         })
